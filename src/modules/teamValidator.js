4cd69cc7b819f5e8af327a30158e3e769c20140b
Tussis <andreab88@hotmail.it>
2026-01-26 23:17:47 +0100

    andato avanti con la funz team Validator

--- src/modules/teamValidator.js
+++ src/modules/teamValidator.js
@@ -1,18 +1,32 @@
-import {deckValidator} from "./deckValidator";
+import { deckValidator } from "./deckValidator";
 
 async function teamValidator(deck1, deck2, deck3) {
-    const results = await Promise.all([deckValidator(deck1), deckValidator(deck2), deckValidator(deck3)]);
+  const results = await Promise.all([deckValidator(deck1), deckValidator(deck2), deckValidator(deck3)]);
 
-    const errors = [];
-    results.forEach((result) => {errors.push(result.errors);});
+  const errors = [];
+  const decks = [];
+  results.forEach((result) => {
+    errors.push(result.errors);
+    if (result.cardsList) decks.push(result.cardsList);
+  });
 
-    if (errors.every(i => i.length > 0)) {
-        console.table(...errors);
-    } else {
-        const cardSet = new Set();
-        console.log(...results);
-    }
-    // return qualcosa?!
+  if (errors.some(i => i.length > 0)) {
+    console.table(errors);
+    return errors;
+  }
+
+  // da rivedere sta parte.. 
+  // Provare a fare un ciclo che controlla tutti i singoli array e se trova un doppione pusha quell'elemento in errors nelle posizioni corrette con la solita struttura [[][][]]
+  const cardSet = new Set();
+  const cardArray = [];
+  results.forEach(result => {
+    result.cardsList.forEach(card => {
+      cardSet.add(card);
+      cardArray.push(card);
+    });
+  });
+  console.log(...results);
+  if (cardArray.length === cardSet.size) console.log("valid");
 }
 
 export { teamValidator };
